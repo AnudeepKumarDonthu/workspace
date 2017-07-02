e2eda0529aaa844f7ae0c551f9c3f7121bd5bdcd
Anudeep Kumar Donthu <anudeepdonthu@gmail.com>
2017-07-02 10:02:22 +0530

    practice

---
+++ myoneplace/src/main/java/com/oneplace/start/App.java
@@ -0,0 +1,16 @@
+package com.oneplace.start;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class App 
+{
+    public static void main(String[] args)
+    {
+        System.out.println( "Hello World!" );
+    }
+}
