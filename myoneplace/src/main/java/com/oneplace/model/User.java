58a19366a0b79bdb2b54221e4ee390b5817b13bf
Anudeep Kumar Donthu <anudeepdonthu@gmail.com>
2017-07-06 21:10:57 +0530

    practice

---
+++ myoneplace/src/main/java/com/oneplace/model/User.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package com.oneplace.model;
+
+/**
+ * @author AnudeepKumar
+ *
+ */
+public class User {
+
+	private String userName;
+	private String password;
+	private String firstName;
+	private String lastName;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
--- myoneplace/src/main/resources/public/js/application-angular.js
+++ myoneplace/src/main/resources/public/js/application-angular.js
@@ -1,12 +1,16 @@
 var app = angular.module('myApp', []);
-app.controller('controller', function($scope) {
-
+var commonController=app.controller('controller', function($scope) {
 	var image = {
 		src : "../images/oneplace_logo.jpg",
 		text : "One Place"
 	};
-	$scope.firstName = "Anudeep Kumar";
-	$scope.lastName = "Donthu";
 	$scope.image = image;
+});
 
+app.controller('signupcontroller', function($scope) {
+	var image = {
+		src : "../images/oneplace_logo.jpg",
+		text : "One Place"
+	};
+	$scope.image = image;
 });
