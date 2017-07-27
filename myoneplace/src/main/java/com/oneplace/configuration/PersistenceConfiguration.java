dcfa6a3b956bd577c62517b6fda2e359a2d33dbd
Anudeep Kumar Donthu <anudeepdonthu@gmail.com>
2017-07-27 07:34:35 +0530

    practice

--- myoneplace/src/main/java/com/oneplace/configuration/PersistenceConfiguration.java
+++ myoneplace/src/main/java/com/oneplace/configuration/PersistenceConfiguration.java
@@ -3,10 +3,25 @@
  */
 package com.oneplace.configuration;
 
+import javax.sql.DataSource;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * @author AnudeepKumar
  *
  */
+@Configuration
 public class PersistenceConfiguration {
+	
+	
+	@Bean
+	@ConfigurationProperties(prefix="spring.datasource")
+	public DataSource dataSource() {
+		return DataSourceBuilder.create().build();
+	}
 
 }
